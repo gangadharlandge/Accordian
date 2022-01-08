f96cbe7d03ccb8ad024ded5edbca059ccb765fb0
gangadhar <gangadharlandge8@gmail.com>
2022-01-07 22:36:09 -0800

    adding files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {data} from './api';
+import "./app.css"
+import Accordion from './accordion';
+function App() {
+   
+   const[content, setContent] = useState(data)
+   
+
+  return (
+    
+    <div className='wrapper'>
+     {
+       content.map( (currEle, index) => {
+        return(
+          <div className="card" key={index}>
+            <Accordion {...currEle}/>
+            
+        </div>
+        )
+       })
+     }
+        
+    </div>
+  );
+
+}
+export default App;
+
+
+
